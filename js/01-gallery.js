887f3f6aaab20f90bd95f14dfc731751571e9906
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-10-29 20:42:18 +0300

    installing basicLightbox, add scripts to 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,60 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClickCreateModal);
+
+// rendered items
+function createGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join('');
+}
+
+// create modal
+function onImgClickCreateModal(e) {
+    e.preventDefault();
+    // if (e.target.nodeName !== "IMG") {return;}
+
+    const isItemImage = e.target.classList.contains("gallery__image");
+    if (!isItemImage) {
+        return;
+    }
+
+    const currentImgUrl = e.target.dataset.source;
+
+    const instance = basicLightbox.create(
+        `
+		<img src="${currentImgUrl}" width="1280" height="auto"/>
+        `,
+        {
+            onShow: (instance) => {
+                window.addEventListener("keydown", onEscKeyPress);
+            },
+            onClose: (instance) => {
+                window.removeEventListener("keydown", onEscKeyPress);
+            },
+        }
+    );
+    instance.show();
+
+    function onEscKeyPress(event) {
+        const ESC_KEY_CODE = 'Escape';
+        const isEscKey = event.code === ESC_KEY_CODE;
+        if (isEscKey) {
+            instance.close();
+        }
+    }
+}
