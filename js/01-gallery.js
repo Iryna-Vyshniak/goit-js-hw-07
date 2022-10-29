4c8cbbc71497f1dd55b4175ec7119ea2cae6fb48
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-10-29 22:17:13 +0300

    fixed close modal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -48,8 +48,8 @@ function onImgClickCreateModal(e) {
                 window.removeEventListener("keydown", onEscKeyPress);
             },
         }
-    ).show();
-    // instance.show();
+    );
+    instance.show();
 
     function onEscKeyPress(event) {
         const ESC_KEY_CODE = 'Escape';
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+// rendered items
+function createGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    }).join('');
+}
+
+
+
 console.log(galleryItems);
